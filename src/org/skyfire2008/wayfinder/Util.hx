631352222a473c4f576f512c3e2af5edf77799d2
Leonid Darovskikh <leonid.darovskikh@isis-papyrus.com>
2020-03-02 14:05:06 +0100

    region border detection works

---
+++ src/org/skyfire2008/wayfinder/Util.hx
@@ -0,0 +1,11 @@
+package org.skyfire2008.wayfinder;
+
+class Util {
+	public static inline function min(a: Int, b: Int): Int {
+		return a > b ? b : a;
+	}
+
+	public static inline function max(a: Int, b: Int): Int {
+		return a > b ? a : b;
+	}
+}
--- src/org/skyfire2008/wayfinder/path/Node.hx
+++ src/org/skyfire2008/wayfinder/path/Node.hx
@@ -1,22 +1,21 @@
 package org.skyfire2008.wayfinder.path;
 
+import org.skyfire2008.wayfinder.Util;
+
+using Lambda;
+
 class Border {
-	public var x: Int;
-	public var y: Int;
-	public var length: Int;
+	public var x0: Int;
+	public var y0: Int;
+	public var x1: Int;
+	public var y1: Int;
 
-	public function new(x: Int, y: Int, length: Int) {
-		this.x = x;
-		this.y = y;
-		this.length = length;
+	public function new(x0: Int, y0: Int, x1: Int, y1: Int) {
+		this.x0 = x0;
+		this.y0 = y0;
+		this.x1 = x1;
+		this.y1 = y1;
 	}
-}
-
-class Check {
-	public var horizontal: Border = null;
-	public var vertical: Border = null;
-
-	public function new() {}
 }
 
 class Node {
@@ -93,9 +92,9 @@ class Node {
 		return result;
 	}
 
-	public static function makeNodes(tiles: Array<Array<Bool>>): Array<Region> {
-		var width = tiles[0].length;
-		var height = tiles.length;
+	public static function makeNodes(walls: Array<Array<Bool>>): Array<Region> {
+		var width = walls[0].length;
+		var height = walls.length;
 
 		// 2d array, showing where regions are located
 		var regions: Array<Array<Region>> = [for (y in 0...height) [for (x in 0...width) null]];
@@ -106,12 +105,12 @@ class Node {
 			var x = 0;
 			while (x < width) {
 				// if current tile is empty
-				if (!tiles[y][x]) {
+				if (!walls[y][x]) {
 					var tileRegion = regions[y][x];
 					// if current tile not occupied by region
 					if (tileRegion == null) {
-						var region = Node.makeRegion(x, y, regions, tiles);
-						trace('made region $region');
+						var region = Node.makeRegion(x, y, regions, walls);
+						// trace('made region $region');
 						regionList.push(region);
 						x += region.width;
 						// otherwise
@@ -126,26 +125,86 @@ class Node {
 			y++;
 		}
 
-		var checks = [for (y in 0...height) [for (x in 0...width) new Check()]];
+		var ids = regions.map(function(line: Array<Region>) {
+			return line.map(function(r: Region) {
+				return r != null ? r.id : 0;
+			});
+		});
+
+		ids.iter(function(item: Array<Int>) {
+			trace(item);
+		});
+
 		var x = 0;
 		var y = 0;
 		for (region in regionList) {
 			// right
-			x = region.right;
-			y = region.y;
-			while (y < region.bottom + 1) {
-				var check = checks[y][x];
-				if (check.horizontal == null) {} else {
-					y += check.horizontal.length;
+			if (region.right < width) {
+				x = region.right;
+				y = region.y;
+				while (y < region.bottom) {
+					if (walls[y][x]) {
+						y++;
+					} else {
+						var other = regions[y][x];
+						var y0 = Util.max(region.y, other.y);
+						var y1 = Util.min(region.bottom, other.bottom);
+						y = y1;
+						trace('regions ${region.id} and ${other.id} border at x=${region.right} y=($y0, $y1)');
+					}
 				}
-				y++;
 			}
 
 			// bottom
+			if (region.bottom < height) {
+				x = region.x;
+				y = region.bottom;
+				while (x < region.right) {
+					if (walls[y][x]) {
+						x++;
+					} else {
+						var other = regions[y][x];
+						var x0 = Util.max(region.x, other.x);
+						var x1 = Util.min(region.right, other.right);
+						x = x1;
+						trace('regions ${region.id} and ${other.id} border at x=($x0, $x1) y=${region.bottom}');
+					}
+				}
+			}
 
 			// left
+			if (region.x > 0) {
+				x = region.x - 1;
+				y = region.y;
+				while (y < region.bottom) {
+					if (walls[y][x]) {
+						y++;
+					} else {
+						var other = regions[y][x];
+						var y0 = Util.max(region.y, other.y);
+						var y1 = Util.min(region.bottom, other.bottom);
+						y = y1;
+						trace('regions ${region.id} and ${other.id} border at x=${region.x} y=($y0, $y1)');
+					}
+				}
+			}
 
 			// top
+			if (region.y > 0) {
+				x = region.x;
+				y = region.y - 1;
+				while (x < region.right) {
+					if (walls[y][x]) {
+						x++;
+					} else {
+						var other = regions[y][x];
+						var x0 = Util.max(region.x, other.x);
+						var x1 = Util.min(region.right, other.right);
+						x = x1;
+						trace('regions ${region.id} and ${other.id} border at x=($x0, $x1) y=${region.y}');
+					}
+				}
+			}
 		}
 		return regionList;
 	}
